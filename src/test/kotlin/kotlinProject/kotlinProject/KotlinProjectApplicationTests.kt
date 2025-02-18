94708cab8902ee325891397868b9fd5113d786e8
kingyj00 <onshopyj@gmail.com>
2025-02-18 12:48:34 +0900

    Setting

---
+++ src/test/kotlin/kotlinProject/kotlinProject/KotlinProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package kotlinProject.kotlinProject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
